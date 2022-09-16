568523ce612885f95c029c28efe9735841269c5b
pacomastermind <paco@mastermind.ac>
2022-09-16 10:21:08 +0200

    Mensaje de error

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 const nickInput=document.getElementById("nick");
 const tamanoInput=document.getElementById("tamano");
 const formEntrada=document.getElementById("formEntrada");
+const error=document.getElementById("error");
 
 //Funciones de evento
 function comprobarForm(event){
@@ -16,11 +17,13 @@ function comprobarForm(event){
         console.log("No hay nick");
         nickInput.focus();
         event.preventDefault();
+        error.innerText="El campo de nick no puede estar vacío";
         return false;
     }else if(tamanoInput.value=="0"){
         console.log("No se ha seleccionado tamaño de panel");
         tamanoInput.focus();
         event.preventDefault();
+        error.innerText="Se debe seleccionar un tamaño de panel";
         return false;
     }
     return true;
