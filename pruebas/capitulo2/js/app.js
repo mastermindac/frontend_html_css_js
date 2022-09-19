fd8eea3cbcf9ff7657d5095259d435357124b841
pacomastermind <paco@mastermind.ac>
2022-09-19 15:46:53 +0200

    Error para form gamer

--- pruebas/capitulo2/js/app.js
+++ pruebas/capitulo2/js/app.js
@@ -10,6 +10,7 @@ const edad=document.getElementById("edad");
 const videojuego=document.getElementById("videojuego");
 const plataforma=document.getElementById("plataforma");
 const formEntrada=document.getElementById("formEntrada");
+const error=document.getElementById("error");
 
 //Funciones de evento
 function comprobarForm(event){
@@ -18,22 +19,27 @@ function comprobarForm(event){
     {
         nombre.focus();
         event.preventDefault();
+        error.innerText="El campo de nick está vacío";
         return false;
     }else if(email.value.length==0){
         email.focus();
         event.preventDefault();
+        error.innerText="El campo de email está vacío";
         return false;
     }else if(videojuego.value.length==0){
         videojuego.focus();
         event.preventDefault();
+        error.innerText="El campo de videojuego está vacío";
         return false;
     }else if((parseInt(edad.value)<=0)||(parseInt(edad.value)>8)){
         edad.focus();
         event.preventDefault();
+        error.innerText="No se ha seleccionado la edad";
         return false;
     }else if(plataforma.value==0){
         plataforma.focus();
         event.preventDefault();
+        error.innerText="No se ha seleccionado la plataforma";
         return false;
     }
     return true;
