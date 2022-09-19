321aa1bef18bd748cf2c46f15bfa9fcfb3b6586c
pacomastermind <paco@mastermind.ac>
2022-09-19 11:06:53 +0200

    LocalStorage vs SessionStorage

--- js/datosUsuario.js
+++ js/datosUsuario.js
@@ -6,9 +6,14 @@
 function datosUsuario(nick){
 
     sessionStorage.setItem('nick',nick.value);
+    //A borrar en otros vídeos
+    localStorage.setItem('nick',nick.value);
 }
 
 function mostrarDatosUsuario(){
     let nick = sessionStorage.getItem('nick');
+    console.log(nick);
+    //A borrar en otros vídeos
+    let nickLocal = localStorage.getItem('nick');
     console.log(nick);
 }
